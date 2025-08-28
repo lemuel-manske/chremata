1e9ccb4dac4bec68bceba74135e7f6d4e07647da
lemuel-manske <lemuelkauemanskede.liz@philips.com>
2025-08-28 11:43:43 -0300

    fix(frontend): remove server package

--- frontend/packages/chremata-components/playwright.config.ts
+++ frontend/packages/chremata-components/playwright.config.ts
@@ -19,10 +19,4 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
   ],
-
-  webServer: {
-    command: 'npm run dev',
-    url: 'http://localhost:5174',
-    reuseExistingServer: !process.env.CI,
-  },
 });
--- frontend/packages/chremata-server/rollup.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'rollup';
-
-import { getConfig } from '@chremata-preset/rollup';
-
-const presetConfig = await getConfig();
-
-export default defineConfig({
-  ...presetConfig,
-});
