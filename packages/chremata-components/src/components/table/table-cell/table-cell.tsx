b8852ef87a6dacb9af20d0aa3f10828657d2e228
lemuel-manske <lemuelkauemanskede.liz@philips.com>
2025-09-15 15:13:30 -0300

    test

--- packages/chremata-components/src/components/table/table-cell/table-cell.tsx
+++ packages/chremata-components/src/components/table/table-cell/table-cell.tsx
@@ -1,24 +1,16 @@
-import { Label } from '../../label/label';
-import { classNames } from '@chremata/utils';
-
 import { type TableCellProps } from './table-cell.types';
 
 export function TableCell(props: TableCellProps) {
-  const { children, colSpan, className, header = false } = props;
+  const { children, colSpan, header = false } = props;
 
   const role = header ? 'columnheader' : 'gridcell';
 
   const Tag = header ? 'th' : 'td';
 
-  const classes = classNames({
-    [className ?? '']: !!className,
-  });
-
   return (
     <Tag
       role={role}
-      colSpan={colSpan}
-      className={classes}>
+      colSpan={colSpan}>
       {children}
     </Tag>
   );
